0930abadfb536ebbe3c23c6cd93ebcf9f47e24ae
Newton Scavazzini <newtonscavazzini@gmail.com>
2017-11-07 16:14:03 -0200

    Correcting parameter name in Settings View

--- app/src/main/java/newscavazzini/similarartists/mvp/view/SettingsActivityView.java
+++ app/src/main/java/newscavazzini/similarartists/mvp/view/SettingsActivityView.java
@@ -2,5 +2,5 @@ package newscavazzini.similarartists.mvp.view;
 
 public interface SettingsActivityView {
     void showAppVersion(String version);
-    void openWebsite(String buttonTitle);
+    void openWebsite(String url);
 }
